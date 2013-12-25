d90df166d975f4e3f18c9a0b93ec95d2211bd43f
Richo Healey <richo@psych0tik.net>
2013-12-24 22:29:58 -0800

    Create dongcoin

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Dongcoins");
+    case mBTC: return QString("Milli-Dongcoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Dongcoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
